0e953df456942e9c419dd1d9d081a0f3330a0bc5
lucaschimweg <luca@schimweg.net>
2021-03-03 12:18:59 +0100

    KU-96 Change route-processor protocol to fit api-server

--- api-server/api-server.pb.go
+++ api-server/api-server.pb.go
@@ -4,14 +4,13 @@
 // 	protoc        v3.14.0
 // source: api-server.proto
 
-package route_processor
+package api_server
 
 import (
 	proto "github.com/golang/protobuf/proto"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
-	sync "sync"
 )
 
 const (
@@ -25,210 +24,24 @@ const (
 // of the legacy proto package is being used.
 const _ = proto.ProtoPackageIsVersion4
 
-type RouteProcessorStatus int32
-
-const (
-	RouteProcessorStatus_OK    RouteProcessorStatus = 0
-	RouteProcessorStatus_ERROR RouteProcessorStatus = 1
-)
-
-// Enum value maps for RouteProcessorStatus.
-var (
-	RouteProcessorStatus_name = map[int32]string{
-		0: "OK",
-		1: "ERROR",
-	}
-	RouteProcessorStatus_value = map[string]int32{
-		"OK":    0,
-		"ERROR": 1,
-	}
-)
-
-func (x RouteProcessorStatus) Enum() *RouteProcessorStatus {
-	p := new(RouteProcessorStatus)
-	*p = x
-	return p
-}
-
-func (x RouteProcessorStatus) String() string {
-	return protoimpl.X.EnumStringOf(x.Descriptor(), protoreflect.EnumNumber(x))
-}
-
-func (RouteProcessorStatus) Descriptor() protoreflect.EnumDescriptor {
-	return file_api_server_proto_enumTypes[0].Descriptor()
-}
-
-func (RouteProcessorStatus) Type() protoreflect.EnumType {
-	return &file_api_server_proto_enumTypes[0]
-}
-
-func (x RouteProcessorStatus) Number() protoreflect.EnumNumber {
-	return protoreflect.EnumNumber(x)
-}
-
-// Deprecated: Use RouteProcessorStatus.Descriptor instead.
-func (RouteProcessorStatus) EnumDescriptor() ([]byte, []int) {
-	return file_api_server_proto_rawDescGZIP(), []int{0}
-}
-
-type RouteProcessorRequest struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	User string `protobuf:"bytes,1,opt,name=user,proto3" json:"user,omitempty"`
-	Data string `protobuf:"bytes,2,opt,name=data,proto3" json:"data,omitempty"`
-}
-
-func (x *RouteProcessorRequest) Reset() {
-	*x = RouteProcessorRequest{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_api_server_proto_msgTypes[0]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *RouteProcessorRequest) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*RouteProcessorRequest) ProtoMessage() {}
-
-func (x *RouteProcessorRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_api_server_proto_msgTypes[0]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use RouteProcessorRequest.ProtoReflect.Descriptor instead.
-func (*RouteProcessorRequest) Descriptor() ([]byte, []int) {
-	return file_api_server_proto_rawDescGZIP(), []int{0}
-}
-
-func (x *RouteProcessorRequest) GetUser() string {
-	if x != nil {
-		return x.User
-	}
-	return ""
-}
-
-func (x *RouteProcessorRequest) GetData() string {
-	if x != nil {
-		return x.Data
-	}
-	return ""
-}
-
-type RouteProcessorResponse struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	Status RouteProcessorStatus `protobuf:"varint,1,opt,name=status,proto3,enum=RouteProcessorStatus" json:"status,omitempty"`
-}
-
-func (x *RouteProcessorResponse) Reset() {
-	*x = RouteProcessorResponse{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_api_server_proto_msgTypes[1]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *RouteProcessorResponse) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*RouteProcessorResponse) ProtoMessage() {}
-
-func (x *RouteProcessorResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_api_server_proto_msgTypes[1]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use RouteProcessorResponse.ProtoReflect.Descriptor instead.
-func (*RouteProcessorResponse) Descriptor() ([]byte, []int) {
-	return file_api_server_proto_rawDescGZIP(), []int{1}
-}
-
-func (x *RouteProcessorResponse) GetStatus() RouteProcessorStatus {
-	if x != nil {
-		return x.Status
-	}
-	return RouteProcessorStatus_OK
-}
-
 var File_api_server_proto protoreflect.FileDescriptor
 
 var file_api_server_proto_rawDesc = []byte{
 	0x0a, 0x10, 0x61, 0x70, 0x69, 0x2d, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72, 0x2e, 0x70, 0x72, 0x6f,
-	0x74, 0x6f, 0x22, 0x3f, 0x0a, 0x15, 0x52, 0x6f, 0x75, 0x74, 0x65, 0x50, 0x72, 0x6f, 0x63, 0x65,
-	0x73, 0x73, 0x6f, 0x72, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x12, 0x0a, 0x04, 0x75,
-	0x73, 0x65, 0x72, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x75, 0x73, 0x65, 0x72, 0x12,
-	0x12, 0x0a, 0x04, 0x64, 0x61, 0x74, 0x61, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x64,
-	0x61, 0x74, 0x61, 0x22, 0x47, 0x0a, 0x16, 0x52, 0x6f, 0x75, 0x74, 0x65, 0x50, 0x72, 0x6f, 0x63,
-	0x65, 0x73, 0x73, 0x6f, 0x72, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x2d, 0x0a,
-	0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0e, 0x32, 0x15, 0x2e,
-	0x52, 0x6f, 0x75, 0x74, 0x65, 0x50, 0x72, 0x6f, 0x63, 0x65, 0x73, 0x73, 0x6f, 0x72, 0x53, 0x74,
-	0x61, 0x74, 0x75, 0x73, 0x52, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x2a, 0x29, 0x0a, 0x14,
-	0x52, 0x6f, 0x75, 0x74, 0x65, 0x50, 0x72, 0x6f, 0x63, 0x65, 0x73, 0x73, 0x6f, 0x72, 0x53, 0x74,
-	0x61, 0x74, 0x75, 0x73, 0x12, 0x06, 0x0a, 0x02, 0x4f, 0x4b, 0x10, 0x00, 0x12, 0x09, 0x0a, 0x05,
-	0x45, 0x52, 0x52, 0x4f, 0x52, 0x10, 0x01, 0x32, 0x51, 0x0a, 0x0e, 0x52, 0x6f, 0x75, 0x74, 0x65,
-	0x50, 0x72, 0x6f, 0x63, 0x65, 0x73, 0x73, 0x6f, 0x72, 0x12, 0x3f, 0x0a, 0x0c, 0x50, 0x72, 0x6f,
-	0x63, 0x65, 0x73, 0x73, 0x52, 0x6f, 0x75, 0x74, 0x65, 0x12, 0x16, 0x2e, 0x52, 0x6f, 0x75, 0x74,
-	0x65, 0x50, 0x72, 0x6f, 0x63, 0x65, 0x73, 0x73, 0x6f, 0x72, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73,
-	0x74, 0x1a, 0x17, 0x2e, 0x52, 0x6f, 0x75, 0x74, 0x65, 0x50, 0x72, 0x6f, 0x63, 0x65, 0x73, 0x73,
-	0x6f, 0x72, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x42, 0x3f, 0x5a, 0x3d, 0x67, 0x69,
-	0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6b, 0x75, 0x6c, 0x79, 0x63, 0x6c, 0x6f,
-	0x75, 0x64, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x63, 0x6f, 0x6c, 0x2f, 0x72, 0x6f, 0x75, 0x74,
-	0x65, 0x2d, 0x70, 0x72, 0x6f, 0x63, 0x65, 0x73, 0x73, 0x6f, 0x72, 0x3b, 0x72, 0x6f, 0x75, 0x74,
-	0x65, 0x5f, 0x70, 0x72, 0x6f, 0x63, 0x65, 0x73, 0x73, 0x6f, 0x72, 0x62, 0x06, 0x70, 0x72, 0x6f,
-	0x74, 0x6f, 0x33,
+	0x74, 0x6f, 0x32, 0x0b, 0x0a, 0x09, 0x41, 0x70, 0x69, 0x53, 0x65, 0x72, 0x76, 0x65, 0x72, 0x42,
+	0x35, 0x5a, 0x33, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6b, 0x75,
+	0x6c, 0x79, 0x63, 0x6c, 0x6f, 0x75, 0x64, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x63, 0x6f, 0x6c,
+	0x2f, 0x61, 0x70, 0x69, 0x5f, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72, 0x3b, 0x61, 0x70, 0x69, 0x5f,
+	0x73, 0x65, 0x72, 0x76, 0x65, 0x72, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
-var (
-	file_api_server_proto_rawDescOnce sync.Once
-	file_api_server_proto_rawDescData = file_api_server_proto_rawDesc
-)
-
-func file_api_server_proto_rawDescGZIP() []byte {
-	file_api_server_proto_rawDescOnce.Do(func() {
-		file_api_server_proto_rawDescData = protoimpl.X.CompressGZIP(file_api_server_proto_rawDescData)
-	})
-	return file_api_server_proto_rawDescData
-}
-
-var file_api_server_proto_enumTypes = make([]protoimpl.EnumInfo, 1)
-var file_api_server_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_api_server_proto_goTypes = []interface{}{
-	(RouteProcessorStatus)(0),      // 0: RouteProcessorStatus
-	(*RouteProcessorRequest)(nil),  // 1: RouteProcessorRequest
-	(*RouteProcessorResponse)(nil), // 2: RouteProcessorResponse
-}
+var file_api_server_proto_goTypes = []interface{}{}
 var file_api_server_proto_depIdxs = []int32{
-	0, // 0: RouteProcessorResponse.status:type_name -> RouteProcessorStatus
-	1, // 1: RouteProcessor.ProcessRoute:input_type -> RouteProcessorRequest
-	2, // 2: RouteProcessor.ProcessRoute:output_type -> RouteProcessorResponse
-	2, // [2:3] is the sub-list for method output_type
-	1, // [1:2] is the sub-list for method input_type
-	1, // [1:1] is the sub-list for extension type_name
-	1, // [1:1] is the sub-list for extension extendee
-	0, // [0:1] is the sub-list for field type_name
+	0, // [0:0] is the sub-list for method output_type
+	0, // [0:0] is the sub-list for method input_type
+	0, // [0:0] is the sub-list for extension type_name
+	0, // [0:0] is the sub-list for extension extendee
+	0, // [0:0] is the sub-list for field type_name
 }
 
 func init() { file_api_server_proto_init() }
@@ -236,46 +49,18 @@ func file_api_server_proto_init() {
 	if File_api_server_proto != nil {
 		return
 	}
-	if !protoimpl.UnsafeEnabled {
-		file_api_server_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*RouteProcessorRequest); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-		file_api_server_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*RouteProcessorResponse); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_api_server_proto_rawDesc,
-			NumEnums:      1,
-			NumMessages:   2,
+			NumEnums:      0,
+			NumMessages:   0,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
 		GoTypes:           file_api_server_proto_goTypes,
 		DependencyIndexes: file_api_server_proto_depIdxs,
-		EnumInfos:         file_api_server_proto_enumTypes,
-		MessageInfos:      file_api_server_proto_msgTypes,
 	}.Build()
 	File_api_server_proto = out.File
 	file_api_server_proto_rawDesc = nil
